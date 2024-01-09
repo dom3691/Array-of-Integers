124dbce2a066a8ec3b567b8e84a25c88878059e9
Dominic James <30402312+dom3691@users.noreply.github.com>
2024-01-09 18:17:07 +0100

    Add project files.

---
+++ Array of Integers/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the number of elements in the array:");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] array = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine($"Enter element {i + 1}:");
+            array[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        Console.WriteLine($"Total Score: {GetScore(array)}");
+    }
+
+    public static int GetScore(int[] array)
+    {
+        int score = 0;
+        foreach (int num in array)
+        {
+            if (num % 2 == 0)
+            {
+                score += 1;
+            }
+            else
+            {
+                score += 3;
+            }
+
+            if (num == 8)
+            {
+                score += 5;
+            }
+        }
+        return score;
+    }
+}
